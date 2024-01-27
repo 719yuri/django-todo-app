9b179e997c6eed8ffc4faff82f5f7f440dde9b48
中村有里 <nakamurayuri@nakamurayurinoMacBook-Air.local>
2024-01-27 19:10:10 +0900

    htmlを・・・

---
+++ todo/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from todo.views import TaskListView
+
+
+urlpatterns = [
+    path('', TaskListView.as_view(), name='task-list')
+]
+
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 # Create your views here.
+from todo.models import Task
+
+class TaskListView(ListView):
+
+  model = Task
+  template_name = 'todo/tasks.html'
+  
