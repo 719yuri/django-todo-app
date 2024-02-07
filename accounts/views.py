aa1df297a08c02226f2589d39e09e8c6e48e0d5c
719yuri <yuri.tennis1205@gmail.com>
2024-02-07 22:54:47 +0900

    [add]ログインログアウト機能の実装(エラー解消途中)

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,17 @@ from django.contrib.auth.views import LoginView
 
 class MyLoginView(LoginView):
     template_name='accounts/login.html',
-    redirect_autheticated_user='True',
+    redirect_authenticated_user='True',
+
+#2/7めも　とりあえずこのauthe~~をauthe n ticated にしたけど変わらず。
+#ちな動画(56)ではN抜け、でも問題なさそうだった…動画の他のとこではN入れてるし、
+#地の文でも入ってるから入れるに越したことはないでしょう。
+
+#追記。タイプエラーとは型がおかしいということ（使えない変数使っちゃってるとか、成立してないみたいな。
+#️今回はタプルじゃダメと言われてる。タプル＝複数の要素が決まった順番？に並んだ値「オブジェクト」のこと。
+#んで、タプルじゃなくてstrとか他の使ってねって言われてる。っぽい。
+#んで、/accounts/login/に問題があるらしい。タイプエラーなのだから紐づいてないとかそういうんではなさそう？
+#ログインと同時に作ったログアウトは問題なかったのになあ。。二つの違いってViewsに切り分けてルーティングしたかどうかくらい。
+#次見てみるときはhttps://prograshi.com/language/python/python-tuple/を見てみようかな。知らない間に勝手にタプルとかこわ。
+#
+
